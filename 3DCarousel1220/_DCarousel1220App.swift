548fe83a5ff877c03632d44f0545577fa828179c
张亚飞 <672233580@qq.com>
2022-12-20 10:22:47 +0800

    Initial Commit

---
+++ 3DCarousel1220/_DCarousel1220App.swift
@@ -0,0 +1,17 @@
+//
+//  _DCarousel1220App.swift
+//  3DCarousel1220
+//
+//  Created by 张亚飞 on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct _DCarousel1220App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
